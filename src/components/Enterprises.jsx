d9e7e515c7f62f9c27eda4cf7c726be5caa235aa
Evgeniy Garanin <dragon135ded@gmail.com>
2025-04-14 22:50:41 +0300

    feat: create navigation and enterprises

---
+++ src/components/Enterprises.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../styles/Navigation.module.scss";
+
+function Enterprises() {
+    return (
+        <>
+            <a href="https://bhp.by/">Брестский хлебокомбинат</a>
+            <a href="https://barhleb.by/">барановичский хлебокомбинат</a>
+            <a href="https://pinskhleb.by/">Пинский хлебокомбинат</a>
+            <a href="https://hlebozavod7.ru/">Березовский хлебокомбинат</a>
+            <a href="">Рыбхозсельд(возможно нет сайта и возможно называется селец)</a>
+            <a href="https://rybhoz-palesye.ibiz.by/">Рыбхоз польесе (возможно нет сайта)</a>
+            <a href="https://dneprobugski.ibiz.by/">Рыбхоз Днепробугский(возможно нет сайта)</a>
+            <a href="https://deal.by/cs/345950">Рыбхоз локтыши (возможно нет сайта)</a>
+        </>
+    );
+}
+
+export default Enterprises
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "../styles/Navigation.module.scss";
+
+function Navigation() {
+    return(
+        <>
+            <nav>
+                <a href="">О нас</a>
+                <a href="">Предприятия</a>
+                <a href="">Продукция</a>
+                <a href="">Лаборатория</a>
+                <a href="">Контакты</a>
+                <a href="">Электронное обращение</a>
+                <a href="">Новости</a>
+                <a href="">Прайс-лист</a>
+            </nav>
+        </>
+    );
+}
+
+export default Navigation
